3e8012ae992982643eb20c9dc44e073ba1dc70e6
Patrick Odenborg <patrick.odenborg@ni.com>
2019-06-26 17:09:41 -0500

    end of part 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 // Project GitHub repo
 // https://github.com/Colt/react-colors/tree/master
 
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedColors[4]}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
+    );
+  }
 }
 
 export default App;
