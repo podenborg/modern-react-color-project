26dea2da66fc782cbd8c59f0c6f262814f82c115
Patrick Odenborg <patrick.odenborg@pearson.com>
2019-07-16 15:34:26 -0500

    Made Palette List responsive

--- src/styles/sizes.js
+++ src/styles/sizes.js
@@ -7,7 +7,8 @@ export default {
       xs: '500.98px',
       sm: '600.98px',
       md: '800.98px',
-      lg: '1000.98px'
+      lg: '1000.98px',
+      xl: '1600px',
     };
     return `@media (max-width: ${sizes[size]})`;
   }
